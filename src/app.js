af4cf9dc47163f4caf0fbd0b6dcf875dafc43094
Facu-2022 <facundocorrea1122@gmail.com>
2023-06-19 12:55:35 -0300

    tercera preentrega

---
+++ src/app.js
@@ -0,0 +1,54 @@
+import express from 'express'
+import ProductManager from '../producto.js'
+
+
+
+const app = express()
+
+app.use(express.json())
+
+
+
+const productManager = new ProductManager('src/productos_new/productos.json')
+
+
+
+app.get('/products', async (req, res) => {
+    
+    const limit = req.query.limit
+
+    const products = await productManager.getProducts()
+
+    if (limit < products.length) products.length = limit
+
+    res.json(products)
+
+
+
+
+})
+
+app.get('/products/:pid', async (req, res) => {
+
+    try {
+
+        const pid = parseInt(req.params.pid)
+
+        const product = await productManager.getProductById(pid)
+
+       if (product){
+        res.json(product)
+       }else{
+        res.status(404).json({message: 'Product not found'})
+       }
+
+    } catch (error) {
+
+        res.status(500).json({message : "error.message"})
+    }
+
+})
+
+
+
+app.listen(8080, () => console.log("programa corriendo perfectamente"))
